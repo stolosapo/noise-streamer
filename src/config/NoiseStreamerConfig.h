11443967e1d1fcb9c338fd83534ee49f645efdde
Apostolos Krionidis <akrionidis@vivantehealth.com>
2022-06-27 12:00:32 +0300

    Reformat

--- src/config/NoiseStreamerConfig.h
+++ src/config/NoiseStreamerConfig.h
@@ -9,20 +9,20 @@ using namespace std;
 struct NoiseStreamerConfig
 {
     string hostname;
-	string port;
-	string username;
-	string password;
-	string protocol;
-	string mountpoint;
-	string name;
-	string genre;
-	string description;
-	string url;
-	string isPublic;
-	string bitrate;
-	AudioBitrate typedBitrate;
-	string samplerate;
-	string channels;
+    string port;
+    string username;
+    string password;
+    string protocol;
+    string mountpoint;
+    string name;
+    string genre;
+    string description;
+    string url;
+    string isPublic;
+    string bitrate;
+    AudioBitrate typedBitrate;
+    string samplerate;
+    string channels;
 };
 
 #endif // NoiseStreamerConfig_h__
