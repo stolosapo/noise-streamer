af2f812b3597a3248e96b1e3ab0e55aab8d33c44
Apostolos Krionidis <akrionidis@vivantehealth.com>
2022-07-12 11:40:37 +0300

    Added default values in arguments

--- src/argument/NoiseStreamerArgument.cpp
+++ src/argument/NoiseStreamerArgument.cpp
@@ -34,20 +34,20 @@ string NoiseStreamerArgument::title()
 
 void NoiseStreamerArgument::registerArguments()
 {
-    registerArg(LOGLEVEL, "The LogLevel: TRACE, DEBUG, INFO, WARN, ERROR, FATAL Default: INFO");
-    registerArg(HOSTNAME, "The Icecast hostname");
-    registerArg(PORT, "The Icecast port");
-    registerArg(USERNAME, "The Mountpoint username");
+    registerArg(LOGLEVEL, "The LogLevel: TRACE, DEBUG, INFO, WARN, ERROR, FATAL. Default: INFO");
+    registerArg(HOSTNAME, "The Icecast hostname. Default: '127.0.0.1'");
+    registerArg(PORT, "The Icecast port. Default: '8000'");
+    registerArg(USERNAME, "The Mountpoint username. Default: 'source'");
     registerArg(PASSWORD, "The Mountpoint password");
     registerArg(MOUNTPOINT, "The Mountpoint itself");
     registerArg(NAME, "The Mountpoint Name");
     registerArg(GENRE, "The Mountpoint Genre");
     registerArg(DESCRIPTION, "The Mountpoint Description");
     registerArg(URL, "A Url for the radio station");
-    registerArg(ISPUBLIC, "If Mountpoint is Public, true - false");
-    registerArg(BITRATE, "The Mountpoint Bitrate");
-    registerArg(SAMPLERATE, "The Mountpoint Samplerate");
-    registerArg(CHANNELS, "The Mountpoint Number of channels");
+    registerArg(ISPUBLIC, "If Mountpoint is Public. Default: false");
+    registerArg(BITRATE, "The Mountpoint Bitrate. Default: '128'");
+    registerArg(SAMPLERATE, "The Mountpoint Samplerate. Default: '44100'");
+    registerArg(CHANNELS, "The Mountpoint Number of channels. Default: '2'");
 }
 
 bool NoiseStreamerArgument::noArgs()
@@ -76,6 +76,11 @@ LogLevel NoiseStreamerArgument::getLogLevel()
 
 string NoiseStreamerArgument::getHostname()
 {
+    if (!hasArg(HOSTNAME))
+    {
+        return "127.0.0.1";
+    }
+
     string s = getStringValue(HOSTNAME);
     if (s == "")
     {
@@ -86,6 +91,11 @@ string NoiseStreamerArgument::getHostname()
 
 string NoiseStreamerArgument::getPort()
 {
+    if (!hasArg(PORT))
+    {
+        return "8000";
+    }
+
     string s = getStringValue(PORT);
     if (s == "")
     {
@@ -96,6 +106,11 @@ string NoiseStreamerArgument::getPort()
 
 string NoiseStreamerArgument::getUsername()
 {
+    if (!hasArg(USERNAME))
+    {
+        return "source";
+    }
+
     string s = getStringValue(USERNAME);
     if (s == "")
     {
@@ -146,11 +161,21 @@ string NoiseStreamerArgument::getUrl()
 
 bool NoiseStreamerArgument::getIsPublic()
 {
+    if (!hasArg(ISPUBLIC))
+    {
+        return false;
+    }
+
     return getBoolValue(ISPUBLIC);
 }
 
 int NoiseStreamerArgument::getBitrate()
 {
+    if (!hasArg(BITRATE))
+    {
+        return 128;
+    }
+
     int s = getIntValue(BITRATE);
     if (s == 0)
     {
@@ -161,6 +186,11 @@ int NoiseStreamerArgument::getBitrate()
 
 string NoiseStreamerArgument::getSamplerate()
 {
+    if (!hasArg(SAMPLERATE))
+    {
+        return "44100";
+    }
+
     string s = getStringValue(SAMPLERATE);
     if (s == "")
     {
@@ -171,6 +201,11 @@ string NoiseStreamerArgument::getSamplerate()
 
 string NoiseStreamerArgument::getChannels()
 {
+    if (!hasArg(CHANNELS))
+    {
+        return "2";
+    }
+
     string s = getStringValue(CHANNELS);
     if (s == "")
     {
